50a7f4f8c33a11656ca4b25d34a3cdcce9b35096
Malik <malik.hassanaly@gmail.com>
2024-11-14 07:36:26 -0700

    enable python 3.9

--- setup.py
+++ setup.py
@@ -19,10 +19,11 @@ setup(
     classifiers=[
         "Intended Audience :: General",
         "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.9",
     install_requires=install_requires,
 )
