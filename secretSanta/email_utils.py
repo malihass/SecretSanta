03d6c8e110b82be6f59bd866d9a79d91bf720ef1
Malik <malik.hassanaly@gmail.com>
2025-11-19 07:47:35 -0700

    option to add an image

--- secretSanta/email_utils.py
+++ secretSanta/email_utils.py
@@ -1,24 +1,70 @@
 import imaplib
+import os
 import smtplib
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
 def send_email(
-    email_address, message_body, subject, host_email_address, host_pwd
+    email_address: str,
+    message_body: str,
+    subject: str,
+    host_email_address: str,
+    host_pwd: str,
+    image_path: str = None,
 ):
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
     server.login(host_email_address, host_pwd)
-    msg = MIMEMultipart("alternative")
+
+    # Are we including an image?
+    embedded_image = False
+    if image_path is not None and os.path.isfile(image_path):
+        embedded_image = True
+
+    # Make sure to use the related is important if image is used
+    if embedded_image:
+        msg = MIMEMultipart("related")
+    else:
+        msg = MIMEMultipart("alternative")
+
     msg["Subject"] = subject
     msg["From"] = "The Secret Santa Corporation"
     msg["To"] = email_address
 
-    part1 = MIMEText(message_body, "html")
-    msg.attach(part1)  # text must be the first one
+    if embedded_image:
+        msg_alt = MIMEMultipart("alternative")
+        msg.attach(msg_alt)
+        # HTML body with reference to image via cid
+        html_with_image = f"""
+        <html>
+        <body>
+            {message_body}
+            <br><br>
+            <div style="text-align:center;">
+                <img src="cid:embedded_image" style="max-width:400px;">
+            </div>
+            <br>
+            PS: You may consult our refactored code here: https://github.com/malihass/SecretSanta  
+
+        </body>
+        </html>
+        """
+        msg_alt.attach(MIMEText(html_with_image, "html"))
+
+        # Attach image with Content-ID
+        with open(image_path, "rb") as f:
+            img = MIMEImage(f.read())
+            img.add_header("Content-ID", "<embedded_image>")
+            img.add_header("Content-Disposition", "inline")
+            msg.attach(img)
+    else:
+        part1 = MIMEText(message_body, "html")
+        msg.attach(part1)  # text must be the first one
+
     server.sendmail(host_email_address, email_address, msg.as_string())
 
     print(" Email has been sent ")
